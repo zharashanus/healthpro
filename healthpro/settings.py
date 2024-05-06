feac7b16b95eb52371c5760debaf624548dc940c
Zharashan <116307474+zharashanus@users.noreply.github.com>
2024-05-06 13:44:05 +0500

    changed settings

--- healthpro/settings.py
+++ healthpro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ppd*m!cc8w@-l#j8v)67#!f_kyvje8$3z5shaf51#h7_v%n(3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com']
+ALLOWED_HOSTS = ['zharashan.pythonanywhere.com',]
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
-
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
