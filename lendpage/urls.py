4483a4689d8a84934b9a9f2feb14abb0c431ee61
Zharashan <116307474+zharashanus@users.noreply.github.com>
2024-05-06 13:49:27 +0500

    somechange1349

--- lendpage/urls.py
+++ lendpage/urls.py
@@ -7,5 +7,4 @@ from lendpage import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_page, name='main_page'),
-    path('my/', views.my_page, name='my_page'),
 ]
